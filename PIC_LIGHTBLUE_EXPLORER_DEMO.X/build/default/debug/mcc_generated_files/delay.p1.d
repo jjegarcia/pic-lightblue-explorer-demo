c00d63aabe8ec0d7e2a7f51964d02b4e0b422d9b
Jenaro Garcia <jjegarcia@hotmail.com>
2025-03-28 13:24:07 +0000

    replaced static with volatile for feature flags

---
+++ PIC_LIGHTBLUE_EXPLORER_DEMO.X/build/default/debug/mcc_generated_files/delay.p1.d
@@ -0,0 +1,2 @@
+build/default/debug/mcc_generated_files/delay.p1:  \
+mcc_generated_files/delay.c 
---
+++ PIC_LIGHTBLUE_EXPLORER_DEMO.X/build/default/debug/mcc_generated_files/i2c1_driver.p1.d
@@ -0,0 +1,3 @@
+build/default/debug/mcc_generated_files/i2c1_driver.p1:  \
+mcc_generated_files/i2c1_driver.c  \
+mcc_generated_files/i2c1_driver.h 
